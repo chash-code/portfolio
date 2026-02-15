5990f7c84554922ed0c1bed1116a5ce4b940ec95
chash-code <panchalchash@gmail.com>
2026-02-15 23:30:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This makes it work on GitHub Pages
+  base: '/portfolio/', 
 })
